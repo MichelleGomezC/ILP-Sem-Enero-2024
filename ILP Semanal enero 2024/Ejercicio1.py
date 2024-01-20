49e54849d409443aadc521c264eace8047ad1f3b
MichelleGomezC <156980533+MichelleGomezC@users.noreply.github.com>
2024-01-19 19:03:56 -0600

    Ejercicio 1,2,3

---
+++ ILP Semanal enero 2024/Ejercicio1.py
@@ -0,0 +1,22 @@
+# Ejercicio 1. Calcular el promedio de 3 calificaciones y decir si es aprobado o reprobado.
+
+# Importar librerias o bibliotecas
+
+# ENTRADA DE DATOS
+# Declarar, crear o instanciar variables
+calificación_1 = float(input("Ingresa la primera calificación: "))
+calificación_2 = float(input("Ingresa la segunda calificación: "))
+calificación_3 = float(input("Ingresa la tercera calificación: "))
+
+
+# PROCESOS (Operaciones y/o cálculos matemáticos y lógicos)
+# JERARQUÍA DE OPERADORES
+# 1 pot y raíz
+# 2 mult y div
+# 3 suma y resta
+promedio = (calificación_1 + calificación_2 + calificación_3) / 3
+
+
+# SALIDA DE DATOS
+print("El promedio es: ", round(promedio, 2))
+
